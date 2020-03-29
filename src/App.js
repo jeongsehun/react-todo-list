68beb1232b23226f6ecd7496265a7e2a358cc744
jeongsehun <sehun.dev@gmail.com>
2020-03-29 16:22:14 +0900

    init react-todo-list!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import TodoList from './components/TodoList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      todoItems: []
+    }
+  }
+
+  render() {
+    return (
+      <div className="form">
+        <div className="header">
+          <h2>Camel`s TodoList!</h2>
+          <input type="text" placeholder="anything write here"></input>
+          <span className="add-btn">Add</span>
+        </div>
+        <TodoList></TodoList>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './TodoList.css';
+
+class TodoList extends Component {
+
+  render() {
+    return (
+      <ul>
+        <li>TodoList Items</li>
+      </ul>
+    )
+  }
+}
+
+export default TodoList;
